a274c8471d42bbbe43f1c2e68b50b061ab377f7d
Natthasak Vechprasit <codesonteen@users.noreply.github.com>
2018-09-06 00:13:07 +0900

    Update run.php

--- run.php
+++ run.php
@@ -22,16 +22,18 @@ $sth = $pdo->query($sql);
 $sth->setFetchMode(PDO::FETCH_OBJ);
 
 while($group = $sth->fetch()) {
+    // Please change the following value map to your settings and your database schema.
     $account->username    = $group->unixuser;
     $account->password    = $group->password;
-    $account->email       = 'bis301@st.tni.ac.th';
+    $account->email       = 'bis301@st.tni.ac.th'; // You can use constant value or use an email of someone in the group.
     $account->package     = 'default';
     $account->firstName   = $group->group_name;
     $account->lastName    = 'BIS-301';
 
     $web->domain = "bis301-$group->subdomain.st.tni.ac.th";
 
-    $database->db_name    = 'web';
+    // VestaCP has default database and username prefix, e.g. groupname_web
+    $database->db_name    = 'web'; 
     $database->db_user    = 'web';
     $database->db_pass    = $group->password;
     $database->db_host    = 'db.st.tni.ac.th' // db.st.tni.ac.th OR 172.16.55.101
